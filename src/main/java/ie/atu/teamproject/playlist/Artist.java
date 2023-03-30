c3b93d20dcc93fe044cd20614f61ca2519951ec2
Mark Veerasingam <markveer70@gmail.com>
2023-03-30 12:46:10 +0100

    retry of commit push

--- src/main/java/ie/atu/teamproject/playlist/Artist.java
+++ src/main/java/ie/atu/teamproject/playlist/Artist.java
@@ -1,53 +1,11 @@
 package ie.atu.teamproject.playlist;
 
-public class Artist extends Playlist{
+public class Artist extends Playlist {
     private int age;
-    private String realName;
-    private String artistName;
 
-    public Artist() {
+    public Artist(int age) {
         super();
-        realName = "";
         age = 0;
-        artistName = "";
     }
 
-    public Artist(int age, String realName, String artistName) {
-        super();
-        this.age = age;
-        this.realName = realName;
-        this.artistName = artistName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
-
-    public String getArtistName() {
-        return artistName;
-    }
-
-    public void setArtistName(String artistName) {
-        this.artistName = artistName;
-    }
-
-    @Override
-    public String toString() {
-        return "Artist Details: " + "\n" +
-                "Artist: " + getArtistName() + "\n" +
-                "Real Name: " + getRealName() + "\n" +
-                "Age: " + getAge();
-    }
 }
