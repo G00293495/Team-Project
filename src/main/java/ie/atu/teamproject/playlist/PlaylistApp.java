7c2cd6ae01176f1357999dec9a7f58a4faae8793
G00403126 <G00403126@atu.ie>
2023-04-17 16:37:08 +0100

    new branch feature_JDBC

--- src/main/java/ie/atu/teamproject/playlist/PlaylistApp.java
+++ src/main/java/ie/atu/teamproject/playlist/PlaylistApp.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class PlaylistApp {
     private static Playlist playlist = new Playlist();
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String choice = "y";
 
