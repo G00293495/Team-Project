8d37b2f113d4b74b23d8c08268b4ae931b88ea92
Mark Veerasingam <markveer70@gmail.com>
2023-04-23 10:59:43 +0100

    Arraylists were also not used, need to look at reimplementation

--- src/main/java/ie/atu/teamproject/playlist/Playlist.java
+++ src/main/java/ie/atu/teamproject/playlist/Playlist.java
@@ -5,22 +5,11 @@ import java.util.ArrayList;
 public class Playlist implements Playlistable {
     private String artistName;
     private String songNames;
-    private ArrayList<ArtistDetails> artistDetails;
 
     public Playlist() {
         super();
         artistName = "";
         songNames = "";
-        artistDetails = new ArrayList<ArtistDetails>();
-    }
-
-    // getter setter for the ArrayList
-    public ArrayList<ArtistDetails> getArtists() {
-        return artistDetails;
-    }
-
-    public void setArtists(ArrayList<ArtistDetails> artistDetails) {
-        this.artistDetails = artistDetails;
     }
 
     public String getArtistName() {
