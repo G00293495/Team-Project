4a85f8527c028836ab9d9134acb6bb17b41216a7
Paddy Downey <G00392495@atu.ie>
2023-04-27 13:00:58 +0100

    Added Remove and add song test - Working

---
+++ src/Test/java/SongTest.java
@@ -0,0 +1,56 @@
+import ie.atu.teamproject.playlist.Song;
+import org.junit.Test;
+
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SongTest {
+    static Connection conn;
+
+    //establish connection
+    static {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlserver://playlistserver.database.windows.net:1433;database=PlaylistExplorerDB;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", "playlistAdmin", "password1.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testAddMedia() throws Exception {
+        String testName = "testSong";
+        Song testSong = new Song(conn);
+
+        testSong.setSongName(testName);
+
+        testSong.addMedia();
+
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM SONG WHERE songName = '" + testName + "'");
+
+        while (resultSet.next())
+        {
+            String name = resultSet.getString("songName");
+            assertEquals(testName,name);
+        }
+    }
+    @Test
+    public void testRemoveMedia() throws Exception{
+        String testName = "testSong";
+        Song testSong = new Song(conn);
+
+        testSong.setSongName(testName);
+
+        testSong.removeMedia();
+
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM SONG WHERE songName = '" + testName + "'");
+
+        while (resultSet.next())
+        {
+            String name = resultSet.getString("songName");
+            assertEquals(testName,name);
+        }
+    }
+}
